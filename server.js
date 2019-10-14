f6fa757dc8e38591ef3a64228ee912c12120c068
Alexander B <r3disrussian@gmail.com>
2019-10-14 20:55:56 +0800

    refactor: 💡 slightly refactored codebase

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
-const WebSocket = require('ws')
+require('dotenv').config()
+
+const { Server, OPEN } = require('ws')
 const fetch = require('node-fetch')
-const config = require('./config')
 const getDifference = require('diff-array-objs')
 
-const server = new WebSocket.Server({
-  port: config.PORT,
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN,
+const COUNT = Number(process.env.COUNT) || 10,
+const REFRESH_DELAY = Number(process.env.REFRESH_DELAY) * 1000 || 20000,
+const PORT = Number(process.env.PORT) || 3000,
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS !== '*' ? process.env.ALLOWED_ORIGINS.split(',') : '*',
+const DEBUG_MODE = process.env.DEBUG_MODE != 1
+
+const log = message => DEBUG_MODE && console.log(`[${new Date().toLocaleString()}] ${message}`, ...arguments)
+
+const server = new Server({
+  port: PORT,
   verifyClient: info =>
-    config.ALLOWED_ORIGINS === '*' || config.ALLOWED_ORIGINS.includes(info.origin)
+    ALLOWED_ORIGINS === '*' || ALLOWED_ORIGINS.includes(info.origin)
 })
 
 /**
  * Получение фото
  */
-const fetchData = () =>
-  new Promise ((resolve, reject) => {
-    console.log(`[${new Date().toLocaleString()}] Receiving new instagram data`)
-    let url = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${config.ACCESS_TOKEN}&count=${config.COUNT}`
-    fetch(url)
-      .then(r => r.json())
-      .then(result => {
-        if (result.meta.code === 200) {
-          resolve(result.data)
-        } else {
-          reject(result)
-        }
-      }).catch(reject)
-  })
+const fetchData = () => new Promise((resolve, reject) => {
+  log(`Receiving new instagram data`)
+  fetch(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${ACCESS_TOKEN}&count=${COUNT}`)
+    .then(r => r.json())
+    .then((result) => result.meta.code === 200 ? resolve(result.data) : reject(result)
+      .catch(reject))
+})
 
-let storedData
+let storedData;
 
-;(async () => {
+(async () => {
   storedData = await fetchData().catch(console.error)
-})()
+})();
 
 /**
  * Авторефреш и автоотправка новых фото клиентам
@@ -41,30 +44,27 @@ const refreshAndSendData = async () => {
     let data = await fetchData()
     let difference = getDifference(data, storedData, 'id')
     if (difference.added.length || difference.removed.length) {
-      console.log(`[${new Date().toLocaleString()}] Sending the changes to all my clients!`)
-      server.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify(data))
-        }
-      })
+      log(`Sending the changes to all my clients!`)
+      server.clients.forEach(client =>
+        client.readyState === OPEN && client.send(JSON.stringify(data))
+      )
     } else {
-      console.log(`[${new Date().toLocaleString()}] No changes at all!`)
+      log(`No changes at all!`)
     }
-    storedData = data
+    storedData = [...data]
   } catch (error) {
     console.error(error)
   }
 }
 
-server.on('connection', async (ws, req) => {
+server.on('connection', async ws => {
   ws.send(JSON.stringify(storedData))
   ws
     .on('error', console.error)
     .on('message', message => {
-      console.log(message)
+      log(message)
       ws.send(JSON.stringify(storedData))
     })
 })
 
-
-setInterval(refreshAndSendData, config.REFRESH_DELAY)
+setInterval(refreshAndSendData, REFRESH_DELAY)
